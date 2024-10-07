34a030ee2f1d6a45911dc8a51f06d29cd86653da
Pedro Henrique Da Fonseca Do Nascimento <pedro.henrique.nas@hotmail.com>
2024-10-07 01:10:37 +0100

    Realizado a criacao dos JFrames: Criar_Usuario, Entrada e Janela_Principal

--- Projeto_exchange/src/projeto_exchange/Projeto_exchange.java
+++ Projeto_exchange/src/projeto_exchange/Projeto_exchange.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package projeto_exchange;
-
+import view.Janela_Principal;
 /**
  *
  * @author pedro
@@ -15,6 +15,9 @@ public class Projeto_exchange {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Janela_Principal principal = new Janela_Principal();
+        principal.setVisible(true);
+    
     }
     
 }
